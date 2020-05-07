db463ddf90dc09458d985b2ee2aee7e3cd006da6
Mati108 <62069366+Mati108@users.noreply.github.com>
2020-05-07 22:17:00 +0200

    Dodanie testu jednostkowego

---
+++ Tests/ViewModelBaseTests.cs
@@ -0,0 +1,17 @@
+﻿using CalendarApplication.UI.ViewModel;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class ViewModelBaseTests
+    {
+        [Test]
+        public void PropertyChanged_ComparingWithEmptyWord_ViewThatNothingChanges()
+        {
+            ViewModelBase viewModelBase = new ViewModelBase();
+            string changedPropertyName = string.Empty;
+            viewModelBase.PropertyChanged += (sender, args) => changedPropertyName = args.PropertyName;
+            Assert.AreEqual("", changedPropertyName);
+        }
+    }
+}
